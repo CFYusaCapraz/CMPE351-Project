3a46e24cd66b8e97388dd8052a5c03e9def18db0
Yusa Capraz <cyberfreak_1911@outlook.com>
2021-12-12 22:08:06 +0200

    Added functionality to check if the passed arguments for options are txt files.

--- cpuScheduler.c
+++ cpuScheduler.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
 
 /****************
 -This is CMPE 351 Project program
@@ -8,7 +9,7 @@
 */
 
 /*TODO
--Need to implement a way to check if the passed parameters are a txt file
+-Need to implement a way to check if the passed parameters are a txt file (DONE!)
 -Need to implement a linked list structure and funtions
 -Need to implement a queue structure and funtions
 -Need to implement a custom structure for reading the input file
@@ -17,7 +18,7 @@
 //This funtions is used to print programs usage and what arguments are needed to pass
 void print_usage(){
         printf("Usage: cmpe351 -f <*.txt> -o <*.txt>\n");
-        exit(2);
+        exit(1);
 }
 
 int main(int argc, char *argv[]){
@@ -25,14 +26,14 @@ int main(int argc, char *argv[]){
         char* input_filename = NULL;
         char* output_filename = NULL;
 
-        //Here we check if the parameters are passed
-        while((options = getopt(argc, argv, "f:o:")) != -1)
+        //Here we check if the correct options are used
+        while ((options = getopt(argc, argv, "f:o:")) != -1)
         {
                 switch(options)
                 {
                         case 'f':
-                                input_filename = optarg;
-                                break;
+				input_filename = optarg;
+				break;
                         case 'o':
                                 output_filename = optarg;
                                 break;
@@ -42,6 +43,23 @@ int main(int argc, char *argv[]){
                 }
         }
 
+	//Here we check if the arguments are passed for options
+	if (input_filename == NULL || output_filename == NULL)
+	{
+		print_usage();
+	}
+	//If passed we check if they are the correct file type
+	else
+	{
+		char* token_Input = strtok(input_filename, ".");
+		token_Input = strtok(NULL, ".");
+
+		char* token_Output = strtok(output_filename, ".");
+		token_Output = strtok(NULL, ".");
+
+		if (strcmp(token_Input,"txt") || strcmp(token_Output,"txt"))
+			print_usage();
+	}
 
         return 0;
 }
