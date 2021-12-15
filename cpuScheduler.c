f21e0401e6b3d3fe4f6a064471f17a886e0621a5
Yusa Capraz <cyberfreak_1911@outlook.com>
2021-12-15 02:09:49 +0200

    Added a Menu. Further development needed. TODO List updated!

--- cpuScheduler.c
+++ cpuScheduler.c
@@ -13,7 +13,23 @@
 -Need to implement a linked list structure and funtions
 -Need to implement a queue structure and funtions
 -Need to implement a custom structure for reading the input file
+-Need to make a menu (ON PROCESS)
 */
+
+enum CPUMethods{NONE, FCFS, SJF, PS, RR}state=0;//CPU Scheduling Methods Enumeration
+enum PMode{OFF,ON}mode=0;//Preemtive Mode Enumeration
+
+void menu(){
+	int option;
+	printf("\t\tCPU Scheduler Simulator\n");
+	printf("1-) Scheduling Method (%d)\n",state);
+	printf("2-) Preemtive Mode (%d)\n",mode);
+	printf("3-) Show Result\n");
+	printf("4-) End Program\n");
+	printf("Option > "); scanf("%1d",&option);
+
+	//Will implement a switch syntax here
+}
 
 //This funtions is used to print programs usage and what arguments are needed to pass
 void print_usage(){
@@ -48,6 +64,8 @@ int main(int argc, char *argv[]){
 	{
 		print_usage();
 	}
+
+	menu();
 
         return 0;
 }
